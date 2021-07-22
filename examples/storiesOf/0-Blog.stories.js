6bc4e4818a5b6e39aae1c18b16fdc0a473b0b02d
mhatvan <markus_hatvan@aon.at>
2021-07-22 08:21:10 +0200

    - upgrade use-image to v1.0.8 to fix unmet peer dependency warning - fixed deprecated story import glob pattern in main.js - fixed lint error thrown in 0-Blog.stories.js - changed to using named React imports in panel.tsx - gitignore storybook-static folder that is generated through build-storybook

--- examples/storiesOf/0-Blog.stories.js
+++ examples/storiesOf/0-Blog.stories.js
@@ -1,14 +1,16 @@
 import { storiesOf } from "@storybook/react";
 import { BlogIndex, BlogGallery, BlogPost } from "../";
 
+// eslint-disable-next-line no-undef
 storiesOf("Blog", module)
   .addParameters({
     abstract: {
       // Copy a collection or layer share url from Abstract
-      url: "https://share.goabstract.com/733ca894-a4bb-43e3-a2b1-dd27ff6d00c4?mode=build"
-    }
+      url:
+        "https://share.goabstract.com/733ca894-a4bb-43e3-a2b1-dd27ff6d00c4?mode=build",
+    },
   })
-   // Name your stories after layers in the collection
+  // Name your stories after layers in the collection
   .add("Blog Index", () => <BlogIndex />)
   .add("Blog Gallery", () => <BlogGallery />)
   .add("Blog Post", () => <BlogPost />);
